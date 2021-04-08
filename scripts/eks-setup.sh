ffd8cc0b0d9f268a57d39bb65933b5f4089fae53
SonnyRLM <sonnymil123@gmail.com>
2021-04-08 09:44:29 +0000

    added cluster yaml

--- scripts/eks-setup.sh
+++ scripts/eks-setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Script to deploy eks cluster 
-clusterExist=$(eksctl get cluster | grep -w "JenkinsCluster")
+clusterExist=$(eksctl get cluster | grep -w "project-cluster")
 var_length=${#clusterExist}
 
 if (( ${var_length}==0 )); then
