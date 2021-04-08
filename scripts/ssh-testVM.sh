bf2d396e6e968a37603d8da95d9cb98dc473de02
SonnyRLM <sonnymil123@gmail.com>
2021-04-08 21:42:38 +0000

    bugfix

--- scripts/ssh-testVM.sh
+++ scripts/ssh-testVM.sh
@@ -8,6 +8,8 @@ cd CNE-Project-2
 
 git checkout Dev
 
+mysql -h cne-project-db-prod.cisxnnla1kct.eu-west-2.rds.amazonaws.com -P 3306 -u admin -ppassword < database/Create.sql
+
 docker-compose up -d --build
 
 docker exec backend bash -c "pytest tests/ --cov application"
