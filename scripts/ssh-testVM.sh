d3e0155061dc7fc1b0b98ea1a3d373eb27bace26
SonnyRLM <sonnymil123@gmail.com>
2021-04-08 21:23:27 +0000

    bugfix

--- scripts/ssh-testVM.sh
+++ scripts/ssh-testVM.sh
@@ -13,5 +13,8 @@ docker-compose up -d --build
 docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
 docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
 
+cat backend_test.txt
+cat frontend_test.txt
+
 docker-compose down
 
