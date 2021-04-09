53b1fbe8e1fb8424edb7fea0faeafc6eafe992a8
SonnyRLM <sonnymil123@gmail.com>
2021-04-09 00:24:03 +0000

    bugfixes

--- scripts/ssh-testVM.sh
+++ scripts/ssh-testVM.sh
@@ -9,6 +9,7 @@ cd CNE-Project-2
 git checkout Dev
 
 mysql -h cne-project-db-prod.cisxnnla1kct.eu-west-2.rds.amazonaws.com -P 3306 -u admin -ppassword < database/Create.sql
+mysql -h cne-project-db-dev.cisxnnla1kct.eu-west-2.rds.amazonaws.com -P 3306 -u admin -ppassword < database/Create.sql
 
 docker-compose up -d --build
 
